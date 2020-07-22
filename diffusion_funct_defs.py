c764f2af00dad214e80226565701ec7dc951868f
Yoana <66979699+Yoana-G@users.noreply.github.com>
2020-07-21 23:29:54 -0700

    Add files via upload
    attempting to add sinking velocity .....

--- diffusion_funct_defs.py
+++ diffusion_funct_defs.py
@@ -16,11 +16,30 @@ def diffusion_time_stepping(kappa,dz,dt,N0,N):
     Nj:              float: array of values at next time-step
     '''
     Nj = N
-    d = kappa*dt/dz**2
+    d = kappa*dt/dz**2  #second derivative
+    #double asterisk is an exponent (second derivative in this case)
+   
     # Compute arrays for diffusion
     Njp1 = np.append(Nj[1:],N0) # Dirichlet boundary condition (fixed value)
     Njm1 = np.append(Nj[0],Nj[:-1]) # Neumann boundary condition (no-flux)
-    # Time stepping
-    Nj = d*Njp1+(1-2*d)*Nj+d*Njm1
     
+    # Time stepping
+        # the minus 2 comes from the stepping process
+        # the 1 comes from explicit method (Euler's)
+    Nj = d*Njp1+(1-2*d)*Nj+d*Njm1   #second derivative
     return Nj
+
+def sinking_time_stepping(dz,N0,PE):
+    PEj = PE
+    d = 1 / dz
+    
+    # Compute arrays for sinking velocity
+    #PEjp1 = np.append(PEj[1:],PE0) # Dirichlet boundary condition (fixed value)
+    PEjp1 = np.append(PEj[0],PEj[:-1])
+    #Njm1 = np.append(Nj[0],Nj[:-1]) # Neumann boundary condition (no-flux)
+    
+    # Time stepping
+    #Nj = d*Njp1+(1-2*d)*Nj+d*Njm1   #second derivative
+    PEj = d*PEjp1-d*PEj
+    return PEj
+
