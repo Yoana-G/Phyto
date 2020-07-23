0b710149c4e15b84468392a92751a3587c68ffb3
Yoana <66979699+Yoana-G@users.noreply.github.com>
2020-07-23 11:40:28 -0700

    Add files via upload
    changed top boundary condition..

--- diffusion_funct_defs.py
+++ diffusion_funct_defs.py
@@ -29,15 +29,15 @@ def diffusion_time_stepping(kappa,dz,dt,N0,N):
     Nj = d*Njp1+(1-2*d)*Nj+d*Njm1   #second derivative
     return Nj
 
-def diffusion2_time_stepping(kappa,dz,dt,PE0,PE):
+def diffusion2_time_stepping(kappa,dz,dt,PE0,W,PE):
     PEj = PE
     d = kappa*dt/dz**2  #second derivative
     #double asterisk is an exponent (second derivative in this case)
    
     # Compute arrays for diffusion
     PEjp1 = np.append(PEj[1:],PE0) # Dirichlet boundary condition (fixed value)
-    PEjm1 = np.append(PEj[-1:],PEj[:-1]) # Neumann boundary condition (no-flux)
-
+    PEjm1 = np.append(PEj[0]/(1+W*kappa/dz),PEj[:-1]) # Neumann boundary condition (no-flux)
+    
     # Time stepping
         # the minus 2 comes from the stepping process
         # the 1 comes from explicit method (Euler's)
