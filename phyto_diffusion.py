223cbd87ffeb672937da6ef3e33d3c380bcf5bca
Yoana <66979699+Yoana-G@users.noreply.github.com>
2020-08-20 14:50:59 -0700

    Add files via upload

--- phyto_diffusion.py
+++ phyto_diffusion.py
@@ -7,8 +7,8 @@ Created on Wed Jun 24 14:00:25 2020
 import numpy as np
 import matplotlib.pyplot as plt        #import plotting library
 from diffusion_funct_defs import diffusion_time_stepping  #import all files in the same folder called 'diffusion func defs'
-from diffusion_funct_defs import sinking_time_stepping
-from diffusion_funct_defs import diffusion2_time_stepping
+#from diffusion_funct_defs import sinking_time_stepping
+#from diffusion_funct_defs import diffusion2_time_stepping
 
 #all PE --> PS  AND all PS --> PR
 
@@ -16,20 +16,20 @@ from diffusion_funct_defs import diffusion2_time_stepping
 H = 1000; # depth of water (1 = non-dimensional)
 K = 1000; # number of grid points (#20)
 kpar = 0.05; # light decay 
-GS = 5;  #5 #max growth rate (1 phyto per day) ; 0.001 - 10 "realistic range"
-GR = 1.5; #1.5
-GE = 5;
+GS = 15;  #5 #max growth rate (1 phyto per day) ; 0.001 - 10 "realistic range"
+GR = 5; #1.5
+#GE = 5;
 I_0 = 1;   #light intensity (I/I_o); constant (doesn't vary w/season); Watts*m^-2 (light flux)
-L_PS = 5; #gamma;light dependence =>determines Growth rate sensitivity to light levels (0.01)
+L_PS = 50; #gamma;light dependence =>determines Growth rate sensitivity to light levels (0.01)
 L_PR = 250; #unitless (it's in the exponential) / (0.001)
-L_PE = 5;
+#L_PE = 5;
 #k = 0.5;  #nutrient units (micromolar)
 k_PR = 0.25;  #half-saturation parameter; 0.01 - 5 "realistic range" (0.05)
 k_PS = 0.5;  #larger (m^2 / s); =0.01 ///// (0.001)
-k_PE = 0.5;
-mPS = 0.1; #mortality rate ("slow") = day^-1 ; << G_max
-mPR = 0.1; #"quadratic mortality" is an option
-mPE = 0.1;
+#k_PE = 0.5;
+mPS = 0.01; #mortality rate ("slow") = day^-1 ; << G_max
+mPR = 0.001; #"quadratic mortality" is an option
+#mPE = 0.1;
 
 z = np.linspace(0,-H,K) # z array
 I = I_0*np.exp(kpar*z) #light profile (light decays exponentially)
@@ -37,20 +37,21 @@ kappa = 0.001 * 86400 # diffusivity value (m^2 * s^-1); 86400 secs/day; kappa = 
 dz = H/(K-1); # calculate z spacing
 N0 = 22; # bottom boundary condition on nutrient
 N = (N0-2)*np.ones(z.shape) # initial condition
+#N = np.ones(z.shape)
 PS = np.ones(z.shape) #Synechococcus vector
 PR = np.ones(z.shape) #Prochlorococcus vector
-PE = np.ones(z.shape) #Picoeukaryote vector
+#PE = np.ones(z.shape) #Picoeukaryote vector
 PSsave = [PS] #saving initial condition
 PRsave = [PR]
-PEsave = [PE]
+#PEsave = [PE]
 Nsave = [N] # save initial condition
 dt = 0.001 # time step
-nt = 100000 # number of time steps 
+nt = 1000000 # number of time steps 
 time = np.arange(nt+1)*dt   #won't work bc it saves each time
 t_val = np.zeros(z.shape)
 tsave = [t_val]
-W = 1; #sinking rate
-PE0 = 0; # bottom boundary condition for phytoplankton ??
+#W = 1; #sinking rate
+#PE0 = 0; # bottom boundary condition for phytoplankton ??
 
 
 for t in np.arange(nt): #for loop: iterate in time, run specified # of times (nt)
@@ -62,16 +63,16 @@ for t in np.arange(nt): #for loop: iterate in time, run specified # of times (nt
     
     dPR_dt= PR*(GR*(1-np.exp(-L_PR*I))*(N/(k_PR+N)) - mPR)   #produces time scale to reach equilibrium
     
-    dPE_dz = sinking_time_stepping(dz,PE0,PE)
+    #dPE_dz = sinking_time_stepping(dz,PE0,PE)
     #d*PEj-d*PEjm1
         
-    dPE_dt = PE*(GE*(1-np.exp(-L_PE*I))*(N/(k_PE+N)) - mPE - W*dPE_dz)
+    #dPE_dt = PE*(GE*(1-np.exp(-L_PE*I))*(N/(k_PE+N)) - mPE - W*dPE_dz)
     # sinking term --> W*dPE_dz
     
     N = N + dN_dt*dt      #forward in time (explicit method) - Euler's method
     PS = PS + dPS_dt*dt
     PR = PR + dPR_dt*dt
-    PE = PE + dPE_dt*dt
+    #PE = PE + dPE_dt*dt
     
       
 #time-stepping diffusion
@@ -79,14 +80,14 @@ for t in np.arange(nt): #for loop: iterate in time, run specified # of times (nt
     PS = diffusion_time_stepping(kappa,dz,dt,0,PS)
     PR = diffusion_time_stepping(kappa,dz,dt,0,PR)
     #PE = diffusion_time_stepping(kappa, dz, dt, 0, PE)
-    PE = diffusion2_time_stepping(kappa,dz,dt,PE0,W,PE)
+    #PE = diffusion2_time_stepping(kappa,dz,dt,PE0,W,PE)
 
-    if np.mod(t,1000) == 0:   #save every 10 time steps 
+    if np.mod(t,10000) == 0:   #save every 10 time steps 
         Nsave = np.concatenate((Nsave,[N]),axis = 0)
         PSsave = np.concatenate((PSsave,[PS]),axis = 0) #adding [PE] to the matrix, starting w/initial value 
                                                         # axis tells dimension; zero = rows
         PRsave = np.concatenate((PRsave,[PR]),axis=0)
-        PEsave = np.concatenate((PEsave,[PE]),axis=0)
+        #PEsave = np.concatenate((PEsave,[PE]),axis=0)
         tsave = np.concatenate((tsave,[t*dt*np.ones(z.shape)]),axis=0)
         
 
@@ -97,7 +98,7 @@ plt.xlabel('time in days')
 plt.ylabel('depth of water column')
 
 plt.figure()
-plt.pcolor(time,z,PEsave.T)
+plt.pcolor(time,z,PRsave.T)
 plt.colorbar()
 plt.xlabel('time in days')
 plt.ylabel('depth of water column')
@@ -118,7 +119,7 @@ line2, = ax.plot(PSsave[-1,:], z, label='Synechococcus (PS)')
 #plt.plot(PEsave[10000,:], z)   #10,000
 ##plotting profiles w/depth --> PS
 line3, = ax.plot(PRsave[-1,:], z, label='Prochlorococcus (PR)')
-line4, = ax.plot(PEsave[-1,:], z, label='Picoeukaryotes (PE)')
+#line4, = ax.plot(PEsave[-1,:], z, label='Picoeukaryotes (PE)')
 
 ax.legend()
 plt.xlabel('N (uMol/m^3)')  #µ
@@ -138,8 +139,17 @@ line3, = ax.plot(tsave[:,0], PSsave[:,0], label='Synechococcus (PS)')
 #plotting time series @particular depth
 line5, = ax.plot(tsave[:,0], PRsave[:,0], label='Prochlorococcus (PR)')
 #line6, = ax.plot(time, PSsave[:,-1], label='PS (at H)')
-line6, = ax.plot(tsave[:,0], PEsave[:,0], label='Picoeukaryotes (PE)')
+#line6, = ax.plot(tsave[:,0], PEsave[:,0], label='Picoeukaryotes (PE)')
 
 ax.legend()
-plt.xlabel('time (day^-1)')
+plt.xlabel('time (day^-1)') 
 plt.ylabel('N (uMol/m^3)')
+
+# We never see Picoeukaryotes present where Syn is not present -- why? Are they outcompeted by the Pro
+# or not able to survive deeper?
+### Because  P grow quickly at high light levels & Syn at low nutrient concentrations (both near the surface)
+##### I think that Pro also outcompete Picoeukaryotes because:
+# Picoeukaryotes grow quickly at high nutrient concentrations & high light levels BUT Syn
+# grow faster at low nutrient concentrations (near surface)
+    # High light Pro --> grow slightly faster than Syn at low nutrient concentration
+    # Low light Pro --> grows faster in low light levels (where Syn/P might be found)
